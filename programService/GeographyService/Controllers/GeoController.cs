714dbd8522c1f1bc5a1a58eadae2a01ffd7c028e
Krishna Dubey <krishdubey95@gmail.com>
2018-11-05 21:30:04 +0530

    commit

---
+++ programService/GeographyService/Controllers/GeoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeographyService.Controllers
+{
+    class Geography
+    {
+        public String Name { get; set; }
+
+        public int ID { get; set; }
+    }
+    [Route("api/[Controller]")]
+    public class GeoController : Controller
+    {
+        List<Geography> geographies = new List<Geography>();
+        public IActionResult Index()
+        {
+            geographies.Add(new Geography() { ID = 1, Name = "Krishna" });
+            geographies.Add(new Geography() { ID = 2, Name = "Atul" });
+            return new JsonResult(geographies);
+        }
+    }
+}
